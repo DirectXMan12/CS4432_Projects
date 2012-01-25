760035566441df5ea6fed6f266c5a4acabe45df9
Solly Ross <directxman12+gh@gmail.com>
2012-01-25 12:49:39 -0500

    ok, eclipse-ready now

---
+++ src/simpledb/remote/SimpleConnection.java
@@ -0,0 +1,38 @@
+package simpledb.remote;
+
+import java.sql.*;
+import java.util.concurrent.Executor;
+
+/**
+ * An adapter class that wraps RemoteConnection.
+ * Its methods do nothing except transform RemoteExceptions
+ * into SQLExceptions.
+ * @author Edward Sciore
+ */
+public class SimpleConnection extends ConnectionAdapter {
+   private RemoteConnection rconn;
+   
+   public SimpleConnection(RemoteConnection c) {
+      rconn = c;
+   }
+   
+   public Statement createStatement() throws SQLException {
+      try {
+         RemoteStatement rstmt = rconn.createStatement();
+         return new SimpleStatement(rstmt);
+      }
+      catch(Exception e) {
+         throw new SQLException(e);
+      }
+   }
+   
+   public void close() throws SQLException {
+      try {
+         rconn.close();
+      }
+      catch(Exception e) {
+         throw new SQLException(e);
+      }
+   }
+}
+
