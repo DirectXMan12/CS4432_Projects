45f3df883c190ee5dd68b655a63f7edb584c59dc
Solly Ross <directxman12+gh@gmail.com>
2012-01-27 14:56:58 -0500

    basic test setup

---
+++ tests/com/wpi/cs4432/simpledb/tests/performance/BufferMgrPerfomanceTests.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package com.wpi.cs4432.simpledb.tests.performance;
+
+import static org.junit.Assert.*;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.sql.Connection;
+import java.sql.Driver;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import simpledb.remote.RemoteDriver;
+import simpledb.remote.RemoteDriverImpl;
+import simpledb.remote.SimpleDriver;
+import simpledb.server.SimpleDB;
+
+/**
+ * @author directxman12
+ *
+ */
+public class BufferMgrPerfomanceTests
+{
+	static Thread serverThread;
+	static Driver mainDriver;
+	static Connection mainConnection;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		try
+		{
+			LocateRegistry.createRegistry(1099); // default
+		}
+		catch(Exception ex)
+		{
+			
+		}
+		final Object mut = new Object();
+		
+		serverThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try
+				{
+					// configure and initialize the database
+					SimpleDB.init("studentdb");
+					System.out.println("finished init of SimpleDB");
+					// post the server entry in the rmi registry
+					RemoteDriver d = new RemoteDriverImpl();
+					Naming.rebind("simpledb", d);
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		serverThread.start();	
+		
+		mainDriver = new SimpleDriver();
+		String connURL = "jdbc:simpledb://localhost";
+		while(true)
+		{
+			try
+			{
+				mainConnection = mainDriver.connect(connURL, null);
+				System.out.println("cheese");
+				return;
+			}
+			catch(Exception ex)
+			{
+				continue;
+			}
+		}
+	}
+	
+	@Test
+	public void test1()
+	{
+		assertTrue(true);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+		serverThread.stop();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+	
+	}
+}
