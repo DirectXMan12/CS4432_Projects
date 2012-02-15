e0966832004dbcb46479e2d699199dd3c887a7f2
Solly Ross <directxman12+gh@gmail.com>
2012-02-15 14:54:43 -0500

    5000 rows per table

--- tests/com/wpi/cs4432/simpledb/tests/SimpleDBBaseTest.java
+++ tests/com/wpi/cs4432/simpledb/tests/SimpleDBBaseTest.java
@@ -92,7 +92,7 @@ public class SimpleDBBaseTest
 		}
 		catch(SQLException ex)
 		{
-			CreateTestTables.createTestTables(20000, mainConnection);
+			CreateTestTables.createTestTables(5000, mainConnection);
 		}
 	}
 	
