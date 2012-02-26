b925641c1844a718ed8a906feba3899cb7ad8236
Solly Ross <directxman12+gh@gmail.com>
2012-02-26 14:30:05 -0500

    readded base tests after reset

---
+++ tests/com/wpi/cs4432/simpledb/tests/CreateTestTables.java
@@ -0,0 +1,111 @@
+package com.wpi.cs4432.simpledb.tests;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+import simpledb.buffer.BasicBufferMgr;
+import simpledb.buffer.BufferMgr;
+import simpledb.buffer.LRUBasicBufferMgr;
+import simpledb.server.SimpleDB;
+
+
+public class CreateTestTables 
+{
+	public static void createTestTables(int numEntries, Connection conn, boolean addRecords)
+	{
+		Random rand = null;
+		Statement s = null;
+		try
+		{
+			s=conn.createStatement();
+			
+			if (!addRecords) for (int i = 1; i < 6; i++) s.executeUpdate("Create table test"+i+" (a"+i+"1 int, a"+i+"2 int)");
+			
+			BufferMgr.setBasicBuffMgrType(BasicBufferMgr.class);
+			SimpleDB.bufferMgr().resetBasicBufferMgr();
+			
+			if (!addRecords)
+			{
+				//createIndicies(s);
+			}
+			
+			Random randomSeed = new Random();
+			int seed = randomSeed.nextInt(20000);
+		
+			for(int i=1;i<6;i++)
+			{
+				/*if(i!=5)
+				{*/
+					rand=new Random(seed);// ensure every table gets the same data
+					System.out.println("creating table "+i);
+					for (int k = 0; k < 100; k++)
+					{
+						for(int j=0;j<numEntries/100;j++)
+						{
+							s.executeUpdate("insert into test"+i+"(a"+i+"1,a"+i+"2) values("+rand.nextInt(1000)+","+rand.nextInt(1000)+ ");");
+						}
+						System.out.println("created up through row "+(k+1)*numEntries/100);
+					}
+				//}
+				/*else//case where i=5
+				{
+					for(int j=0;j<numEntries/2;j++)// insert 10000(half the size) records into test5
+					{
+						s.executeUpdate("insert into test"+i+" (a51,a52) values("+j+","+j+ ")");
+					}
+				}*/
+		   }
+		} 
+		catch (SQLException e)
+		{
+			System.out.println("Issue creating test tables:");
+			e.printStackTrace();
+		}
+		BufferMgr.setBasicBuffMgrType(LRUBasicBufferMgr.class);
+		SimpleDB.bufferMgr().resetBasicBufferMgr();
+	}
+
+	public static void createIndicies(Statement s) throws SQLException
+	{
+		BufferMgr.setBasicBuffMgrType(BasicBufferMgr.class);
+		SimpleDB.bufferMgr().resetBasicBufferMgr();
+		s.executeUpdate("create sh index idx1 on test1 (a11)");
+		s.executeUpdate("create eh index idx2 on test2 (a21)");
+		s.executeUpdate("create bt index idx3 on test3 (a31)");
+	}
+	
+	public static void addNRows(int numEntries, Connection conn)
+	{
+		Random rand = null;
+		Statement s = null;
+		try
+		{
+			s=conn.createStatement();
+			
+			BufferMgr.setBasicBuffMgrType(BasicBufferMgr.class);
+			SimpleDB.bufferMgr().resetBasicBufferMgr();
+			
+			Random randomSeed = new Random();
+			int seed = randomSeed.nextInt(20000);
+		
+			for(int i=1;i<6;i++)
+			{
+				rand=new Random(seed);// ensure every table gets the same data
+				for(int j=0;j<numEntries;j++)
+				{
+					s.executeUpdate("insert into test"+i+"(a"+i+"1,a"+i+"2) values("+rand.nextInt(1000)+","+rand.nextInt(1000)+ ");");
+				}
+		   }
+		} 
+		catch (SQLException e)
+		{
+			System.out.println("Issue creating test tables:");
+			e.printStackTrace();
+		}
+		BufferMgr.setBasicBuffMgrType(LRUBasicBufferMgr.class);
+		SimpleDB.bufferMgr().resetBasicBufferMgr();
+	}
+}
+
---
+++ tests/com/wpi/cs4432/simpledb/tests/SimpleDBBaseTest.java
@@ -0,0 +1,116 @@
+package com.wpi.cs4432.simpledb.tests;
+
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import simpledb.buffer.BasicBufferMgr;
+import simpledb.buffer.BufferMgr;
+import simpledb.buffer.LRUBasicBufferMgr;
+import simpledb.remote.RemoteDriver;
+import simpledb.remote.RemoteDriverImpl;
+import simpledb.remote.SimpleDriver;
+import simpledb.server.SimpleDB;
+
+
+public class SimpleDBBaseTest
+{
+	static Thread serverThread;
+	static Driver mainDriver;
+	static Connection mainConnection;
+	protected Statement stmt;
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		try
+		{
+			LocateRegistry.createRegistry(1099); // default
+		}
+		catch(Exception ex)
+		{
+			
+		}
+		
+		serverThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try
+				{
+					BufferMgr.setBasicBuffMgrType(BasicBufferMgr.class);
+					// configure and initialize the database
+					SimpleDB.init("studentdb");
+					System.out.println("finished init of SimpleDB");
+					// post the server entry in the rmi registry
+					RemoteDriver d = new RemoteDriverImpl();
+					Naming.rebind("simpledb", d);
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		serverThread.start();	
+		
+		mainDriver = new SimpleDriver();
+		String connURL = "jdbc:simpledb://localhost";
+		boolean b = true;
+		while(b)
+		{
+			try
+			{
+				mainConnection = mainDriver.connect(connURL, null);
+				
+				b = false;
+			}
+			catch(Exception ex)
+			{
+				continue;
+			}
+		}
+		
+		Statement stmt = mainConnection.createStatement();
+		try
+		{
+			stmt.executeQuery("select a51 from test5;");
+			//CreateTestTables.addNRows(1, mainConnection);
+		}
+		catch(SQLException ex)
+		{
+			CreateTestTables.createTestTables(100000, mainConnection, false);
+		}
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+		serverThread.stop();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+		stmt = mainConnection.createStatement();
+	}
+}
